a52bc4025fd81952c826fdb8a52a98d8726ca1f6
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2022-05-01 21:31:23 +0300

    added logging_manager to operate with logging_service

---
+++ logging_manager.py
@@ -0,0 +1,25 @@
+from utils import *
+from logging_service import Logger
+
+
+class LoggingManager:
+    def __init__(self, ports):
+        self.loggers_ports = ports
+        self.active_loggers = []
+
+        
+    def run_loggers(self) -> NoReturn:
+        for port in self.loggers_ports:
+            logger = Logger(port)
+            if logger.success:
+                self.active_loggers.append(logger)
+       
+    def kill_loggers(self,n) -> NoReturn:
+        shutdown_loggers : List[Logger] = random.sample(self.active_loggers, 2)
+        for logger in shutdown_loggers:
+            logger.kill_thread()
+                
+
+if __name__ == '__main__':
+    loggingManager = LoggingManager(LOGGERS_PORTS)
+    loggingManager.run_loggers()

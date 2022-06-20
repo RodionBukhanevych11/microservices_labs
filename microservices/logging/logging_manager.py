13c11b5c524c094d6955f9c94c72541d0773a8ef
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2022-06-21 02:14:10 +0300

    minor

--- microservices/logging/logging_manager.py
+++ microservices/logging/logging_manager.py
@@ -1,18 +1,21 @@
 from microservices.utils.utils import *
 from microservices.logging.logging_service import Logger
+import microservices.utils.consul_utils as cf
 
 
 class LoggingManager:
-    def __init__(self, ports):
-        self.loggers_ports = ports
+    def __init__(self):
+        ports = cf.get_kv("ports")
+        self.loggers_ports = ports["loggers_ports"]
         self.active_loggers = []
-
         
     def run(self) -> NoReturn:
+        print(self.loggers_ports)
         for port in self.loggers_ports:
             logger = Logger(port)
             if logger.success:
                 self.active_loggers.append(logger)
+                
        
     def kill(self,n) -> NoReturn:
         shutdown_loggers : List[Logger] = random.sample(self.active_loggers, 2)
@@ -21,5 +24,5 @@ class LoggingManager:
                 
 
 if __name__ == '__main__':
-    loggingManager = LoggingManager(LOGGERS_PORTS)
+    loggingManager = LoggingManager()
     loggingManager.run()
